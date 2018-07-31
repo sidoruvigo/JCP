48fe343c01394cb9dd000c133091207383a55054
sidoruvigo <37708675+sidoruvigo@users.noreply.github.com>
2018-07-31 13:52:27 +0200

    Add files via upload

---
+++ R/zzz.R
@@ -0,0 +1,8 @@
+
+# On load
+#===================
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("Intro message of the package\u002E
+
+                        SiDOR Group\u002E University of Vigo\u002E")
+}
